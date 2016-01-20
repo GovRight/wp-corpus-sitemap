5f2d00295932c8b9c4b699b6ef7d6dc56929d8fe
Vic Miletsky <victor.miletskiy@gmail.com>
2016-01-21 00:22:18 +0200

    Test bump

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 Plugin Name: GovRight Corpus Sitemap
 Plugin URI: https://github.com/GovRight/wp-corpus-sitemap
 Description: A plugin to help you to create a sitemap containing article urls from a law document related to your site. Check <a href="https://github.com/GovRight/wp-corpus-sitemap">the plugin Github page</a> for details and instructions.
-Version: 1.0.3
+Version: 1.0.4
 Author: GovRight
 Author URI: http://govright.org/
 License: MIT
